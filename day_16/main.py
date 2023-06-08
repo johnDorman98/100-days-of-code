9c710ce9208b534a6e5c53f9345f7ab6f6d85326
John <namrods15@gmail.com>
2023-06-08 12:33:25 +0200

    Working on day 16

---
+++ day_16/main.py
@@ -0,0 +1,16 @@
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+
+# screen = Screen()
+# print(screen.canvheight)
+# timmy.shape("turtle")
+# timmy.color("blue")
+# timmy.penup()
+# timmy.forward(100)
+
+# screen.exitonclick()
+
+import prettytable
+
